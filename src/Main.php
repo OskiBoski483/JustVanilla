b68127c30605f4beddc05cb4d265f12a5b00ab8f
Seriously Not Beluga <oskarniziol@gmail.com>
2023-06-30 13:57:48 +0200

    Better config system

--- src/Main.php
+++ src/Main.php
@@ -3,16 +3,19 @@ namespace Vanilla;
 
 use pocketmine\plugin\PluginBase;
 use Vanilla\Skins\SkinsModule;
+use pocketmine\utils\Config;
 
 class Main extends PluginBase{
 
     private $skins;
     private int $modules = 0;
+    protected Config $enabled;
 
     public function onEnable() : void {
-        $this->saveDefaultConfig();
+        $this->saveResource('enabled.yml');
+        $this->enabled = new Config($this->getDataFolder() . 'enabled.yml', Config::YAML);
         $this->getLogger()->info("The Vanilla plugin has been enabled");
-        if ($this->getConfig()->get("skins")) {
+        if ($this->enabled->get("skins", true)) {
             $this->skins = new SkinsModule;
             $this->skins->enable();
             $this->addLoaded();
@@ -21,7 +24,7 @@ class Main extends PluginBase{
 	}
 
     public function onDisable() : void {
-        if ($this->getConfig()->get("skins")) {
+        if ($this->enabled->get("skins", true)) {
             $this->skins->disable();
         }
     }
