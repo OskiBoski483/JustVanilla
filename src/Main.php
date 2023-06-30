ca2f995e5e03106638dda8d41942b94372653ea3
Seriously Not Beluga <oskarniziol@gmail.com>
2023-06-30 13:45:04 +0200

    Add config

--- src/Main.php
+++ src/Main.php
@@ -7,14 +7,26 @@ use Vanilla\Skins\SkinsModule;
 class Main extends PluginBase{
 
     private $skins;
+    private int $modules = 0;
 
-    public function onEnable() : void{
-        $this->skins = new SkinsModule;
-        $this->skins->enable();
-		$this->getLogger()->info("The Vanilla plugin has been enabled");
+    public function onEnable() : void {
+        $this->saveDefaultConfig();
+        $this->getLogger()->info("The Vanilla plugin has been enabled");
+        if ($this->getConfig()->get("skins")) {
+            $this->skins = new SkinsModule;
+            $this->skins->enable();
+            $this->addLoaded();
+        }
+        $this->getLogger()->info("Loaded " . $this->modules . " modules");
 	}
 
     public function onDisable() : void {
-        $this->skins->disable();
+        if ($this->getConfig()->get("skins")) {
+            $this->skins->disable();
+        }
+    }
+
+    private function addLoaded() : void {
+        $this->modules++;
     }
 }
